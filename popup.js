c730d55af97741e2d40704f578e04eecff234ef3
pankaj <pankaj_046@yahoo.in>
2025-03-01 15:20:35 +0530

    update book mark icon

--- popup.js
+++ popup.js
@@ -8,35 +8,42 @@ document.addEventListener('DOMContentLoaded', function () {
                     let div = document.createElement('div');
                     div.className = "grid-item";
 
-                    // Title
+                    let titleContainer = document.createElement('div');
+                    titleContainer.className = "title-container";
+
+                    let favicon = document.createElement('img');
+                    favicon.src = `https://www.google.com/s2/favicons?domain=${new URL(node.url).hostname}&sz=64`;
+                    favicon.className = "favicon";
+                    favicon.alt = "Favicon";
+
                     let title = document.createElement('h2');
                     title.textContent = node.title;
 
-                    // Button container
+
                     let buttonGroup = document.createElement('div');
                     buttonGroup.className = "button-group";
 
-                    // Open button
                     let openButton = document.createElement('button');
                     openButton.textContent = "Open";
                     openButton.onclick = function () {
                         window.open(node.url, "_blank");
                     };
 
-                    // Remove button
+
                     let removeButton = document.createElement('button');
                     removeButton.textContent = "Remove";
                     removeButton.className = "remove";
                     removeButton.onclick = function () {
                         chrome.bookmarks.remove(node.id, function () {
-                            div.remove(); // Remove from UI
+                            div.remove();
                         });
                     };
 
-                    // Append elements
+                    titleContainer.appendChild(favicon);
+                    titleContainer.appendChild(title);
+                    div.appendChild(titleContainer);
                     buttonGroup.appendChild(openButton);
                     buttonGroup.appendChild(removeButton);
-                    div.appendChild(title);
                     div.appendChild(buttonGroup);
 
                     bookmarksContainer.appendChild(div);
